8244f4370533ca3b174b5a8ed5047d2251dde2cf
Charly Abraham <charly@core.ai>
2022-10-05 19:30:11 +0530

    feat: initail implementation for put, deleteDb,createDb,createTable,get

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,33 +1,46 @@
 import WebSocket from 'ws';
-import {isString, isObject} from "@aicore/libcommonutils";
+import {isString, isObject, isObjectEmpty} from "@aicore/libcommonutils";
+import crypto from "crypto";
+import {COCO_DB_FUNCTIONS} from "@aicore/libcommonutils";
 
 let client = null;
 const WEBSOCKET_ENDPOINT_COCO_DB = '/ws';
+const ID_TO_RESOLVE_REJECT_MAP = {};
 
 export function init(cocoDbServiceEndPoint, authkey) {
     if (!isString(cocoDbServiceEndPoint)) {
         throw new Error('Please provide valid cocoDbServiceEndPoint');
     }
-    if (!isString(authkey) || authkey.split(':').length !== 2) {
+    if (!isString(authkey)) {
         throw new Error('Please provide valid authKey in name:password format');
     }
-    client = new WebSocket(`ws://${authkey}@${cocoDbServiceEndPoint}${WEBSOCKET_ENDPOINT_COCO_DB}`, {
-        perMessageDeflate: false
+    client = new WebSocket(`ws://${cocoDbServiceEndPoint}${WEBSOCKET_ENDPOINT_COCO_DB}`, {
+        perMessageDeflate: false,
+        headers: {
+            Authorization: `Basic ${authkey}`
+        }
     });
     client.on('open', function open() {
-        for (let i = 0; i < 10000; i++) {
+        /*for (let i = 0; i < 10000; i++) {
             client.send(JSON.stringify({fn: 'hello'}));
         }
+         */
+        console.log('connected to server');
 
     });
 
     client.on('message', function message(data) {
         console.log('received: %s', data);
+        receiveMessage(data);
     });
 }
 
 function close() {
     client = null;
+}
+
+function getId() {
+    return crypto.randomBytes(4).toString('hex');
 }
 
 export function sendMessage(message) {
@@ -36,15 +49,36 @@ export function sendMessage(message) {
             reject('Please provide valid Object');
             return;
         }
-        if (!isString(message.id)) {
-            reject('Please provide valid id');
-
+        if (!isString(message.fn) || !(message.fn in COCO_DB_FUNCTIONS)) {
+            reject('please provide valid function name');
+            return;
         }
-
+        const id = getId();
+        message.id = id;
+        ID_TO_RESOLVE_REJECT_MAP[id] = {
+            resolve: resolve,
+            reject: reject
+        };
+        client.send(JSON.stringify(message));
     });
-
 }
 
-init('localhost:5000', 'aladdin:opensesame');
+function receiveMessage(rawData) {
+    const message = JSON.parse(rawData);
+
+    if (!isString(message.id)) {
+        console.error('Server message does not have an Id');
+        return;
+    }
+    const requestResolve = ID_TO_RESOLVE_REJECT_MAP[message.id];
+    if (!isObject(requestResolve)) {
+        console.error(`Client did not send message with Id ${message.id} to server`);
+        return;
+    }
+    const response = message.response;
+    requestResolve.resolve(response);
+    delete ID_TO_RESOLVE_REJECT_MAP[message.id];
+}
+
 
 //close();
